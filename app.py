a85fc20a019b18b5ff8eb5d86f5e0b38bbc97b06
Grace C <99157913+GraceC-339@users.noreply.github.com>
2024-07-10 17:52:07 +0000

    try markers

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route('/data')
+def data():
+    df = pd.read_csv('updated_crime_data.csv')
+    data = df.to_dict(orient='records')
+    return {'data': data}
